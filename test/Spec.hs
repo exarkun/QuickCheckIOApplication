4f94d84c0c3ce8b4184bc6d1fb6dc311e4048648
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-09-03 10:49:00 -0400

    Gradual progression towards something more like what I want maybe

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,22 +1,30 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Main where
 
 import Test.QuickCheck
--- import Test.Hspec.Wai.QuickCheck
+import Test.QuickCheck.Monadic
+import qualified Test.Hspec.Wai.QuickCheck as QuickWai
 import Test.Hspec
 import Test.Hspec.Wai
 import Text.Printf
 import Servant
 import Servant.API
 import Data.Aeson
+import Data.Text.Encoding
+import Data.ByteString.UTF8
+  ( fromString
+  )
 
 data Backend = Backend
 
 openBackend :: IO Backend
-openBackend = return Backend
+openBackend = do
+  print "Opening backend"
+  return Backend
 
 data Acknowledgement = Ok Integer
 
@@ -37,12 +45,70 @@ theAPI = Proxy
 app :: Backend -> Application
 app backend = serve theAPI (theServer backend)
 
-prop :: Property
-prop = property $ \n ->
+prop_a :: Property
+prop_a = property $ \n -> (n :: Integer) /= 123456
+
+prop_b :: Integer -> Property
+prop_b n = monadicIO $
   do
-    backend <- openBackend
-    with (app backend) $
-      post (printf "/%d" (n :: Integer)) `shouldRespondWith` 200
+    assert $ n /= 123456
+
+prop_c :: Backend -> Integer -> Property
+prop_c backend n = monadicIO $
+  do
+    assert $ n /= 123456
+
+post' n =
+  let
+    url = printf "/%d" (n :: Integer)
+    encoded = fromString url
+  in
+    post encoded ""
+
+spec_a :: Spec
+spec_a =
+  with (return (app Backend)) $
+  describe "foo" $
+  it "bar" $
+  post' 5 `shouldRespondWith` 200
+
+spec_b :: Backend -> Spec
+spec_b backend =
+  with (return (app backend)) $
+  describe "foo" $
+  it "bar" $
+  post' 5 `shouldRespondWith` 200
+
+spec_c :: Backend -> Spec
+spec_c backend =
+  with (return (app backend)) $
+  describe "foo" $
+  it "bar" $
+  QuickWai.property $ \n -> post' n `shouldRespondWith` 200
+
+spec_d :: IO Backend -> Spec
+spec_d getBackend =
+  with (getBackend >>= return . app) $
+  describe "foo" $
+  it "bar" $
+  QuickWai.property $ \n -> post' n `shouldRespondWith` 200
+
+spec_e :: IO Backend -> Spec
+spec_e getBackend =
+  before (getBackend >>= return . app) $
+  describe "foo" $
+  it "bar" $
+  QuickWai.property $ \n -> post' n `shouldRespondWith` 200
 
 main :: IO ()
-main = quickCheck prop
+main = do
+  quickCheck prop_a
+  quickCheck prop_b
+  backend <- openBackend
+  quickCheck $ prop_c backend
+
+  hspec spec_a
+  hspec $ spec_b backend
+  hspec $ spec_c backend
+  hspec $ spec_d openBackend
+  hspec $ spec_e openBackend
